ae76208ad67492153c6e02f617956455cd728bc7
Pierre Malafosse <pierre.malafosse@letgo.com>
2019-04-30 18:15:52 +0200

    first commit

---
+++ src/main/java/pierre/tutorial1/ConsumerDemo.java
@@ -0,0 +1,22 @@
+package pierre.tutorial1;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ConsumerDemo {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
+
+        String bootstrapServers = "127.0.0.1:9092"
+        Properties properties = new Properties();
+
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, String)
+
+    }
+}
